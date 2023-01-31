6fc0f392d38f283970202448dfcdc8bb85bddd5a
Elijah Semyonov <elijah.semenov@gmail.com>
2023-01-31 17:42:33 +0100

    Initial Commit

---
+++ InfiniteImageViewer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InfiniteImageViewer
+//
+//  Created by Elijah Semyonov on 31/01/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
